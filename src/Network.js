944fe20fd7a802d7dbcd254847473c0dddda9274
Jake Smith <jake.as97@gmail.com>
2019-12-04 11:54:09 +0000

    corrected nodes being drawn on top of each other

--- src/Network.js
+++ src/Network.js
@@ -7,16 +7,16 @@ class Network extends Component {
         //this.createNetwork = this.createNetwork.bind(this);
     }
     componentDidMount() {
-        this.createNetwork();
+        Network.createNetwork();
     }
     componentDidUpdate() {
-        this.createNetwork();
+        Network.createNetwork();
     }
-    createNetwork() {
+    static createNetwork() {
         // set the dimensions and margins of the graph
-        var margin = {top: 10, right: 50, bottom: 30, left: 50},
+        var margin = {top: 50, right: 50, bottom: 50, left: 50},
             width = 800 - margin.left - margin.right,
-            height = 800 - margin.top - margin.bottom;
+            height = 500 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
         var svg = d3.select("#visualisation")
@@ -44,9 +44,32 @@ class Network extends Component {
             .append("circle")
             .attr("r", 20)
             .style("fill", "#69b3a2");
+
+        // Let's list the force we wanna apply on the network
+        var simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
+            .force("link", d3.forceLink()                               // This force provides links between nodes
+                .id(function(d) { return d.id; })                     // This provide  the id of a node
+                .links(data.links)                                    // and this the list of links
+            )
+            .force("charge", d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+            .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
+            .on("end", ticked);
+
+        // This function is run at each iteration of the force algorithm, updating the nodes position.
+        function ticked() {
+            link
+                .attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+
+            node
+                .attr("cx", function (d) { return d.x+6; })
+                .attr("cy", function(d) { return d.y-6; });
+        }
     }
     render() {
-        this.createNetwork();
+        Network.createNetwork();
         return null
     }
 }
