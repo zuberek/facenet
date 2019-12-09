822d4d739840923a83e007e7214d858019a03f63
zuberek <dabrowski.janek@gmail.com>
2019-12-09 19:39:38 +0000

    deploy build

---
+++ precache-manifest.00f565deab59bd13553a78e12cbd06a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "784712b7e721e826723afa02fa7cc9e9",
+    "url": "/facenet/index.html"
+  },
+  {
+    "revision": "67e5ae1e0da80052d290",
+    "url": "/facenet/static/css/main.fb44590d.chunk.css"
+  },
+  {
+    "revision": "c63fc6afb22c2a066439",
+    "url": "/facenet/static/js/2.46228f1d.chunk.js"
+  },
+  {
+    "revision": "67e5ae1e0da80052d290",
+    "url": "/facenet/static/js/main.08ada5eb.chunk.js"
+  },
+  {
+    "revision": "d033720b59f98c1cbaa3",
+    "url": "/facenet/static/js/runtime-main.3cbfb0e5.js"
+  }
+]);
