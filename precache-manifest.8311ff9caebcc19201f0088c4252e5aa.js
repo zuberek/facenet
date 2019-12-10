304b937432f4000eef35a78027d0ac5064584db3
zuberek <dabrowski.janek@gmail.com>
2019-12-10 13:37:20 +0000

    Final update

---
+++ precache-manifest.8311ff9caebcc19201f0088c4252e5aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe15d2b2b60aac4c43fd07b44098964c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a8dbee622294e42767b",
+    "url": "/static/css/main.45c93c37.chunk.css"
+  },
+  {
+    "revision": "bc0d098c0478b1318dff",
+    "url": "/static/js/2.69264b4d.chunk.js"
+  },
+  {
+    "revision": "0a8dbee622294e42767b",
+    "url": "/static/js/main.fd0c625b.chunk.js"
+  },
+  {
+    "revision": "e143039b097bb91bb4b6",
+    "url": "/static/js/runtime-main.e0d33c12.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae93b713350aeeab16e716eb638aa307.js"
+  "/precache-manifest.8311ff9caebcc19201f0088c4252e5aa.js"
 );
 
 self.addEventListener('message', (event) => {
